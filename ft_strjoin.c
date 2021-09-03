7a088f49987e0cd23d9a9b9cab1fb9f69a2eb20a
julrodri <julrodri@DESKTOP-8G33N25.localdomain>
2021-09-02 23:51:31 -0300

    ate ft_strjoin (ja com ft_itoa e ft_split)

---
+++ ft_strjoin.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julrodri <julrodri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/02 23:28:49 by julrodri          #+#    #+#             */
+/*   Updated: 2021/09/02 23:46:47 by julrodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*r;
+	int		i;
+	size_t	last_index;
+
+	i = 0;
+	j = 0;
+	r = (char *) malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (r == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		r[i] = s1[i];
+		i++;
+	}
+	j = i;
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		r[j + 1] = s2[i];
+		i++;
+	}
+	s1[j + i] = '\0';
+	return (r);
+}
